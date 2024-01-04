4aaf6bdb23f0eb9053dfda8e7b4d8a37029fa964
Marwam Malta <marwamprog.js@gmail.com>
2024-01-04 20:56:49 -0300

    feat: Migration finalizado

--- database/migrations/2024_01_04_232945_create_departaments.php
+++ database/migrations/2024_01_04_232945_create_departaments.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProducts extends Migration
+class CreateDepartaments extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateProducts extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('departaments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->integer('stock');
-            $table->double('price');
             $table->string('description')->nullable();
             $table->timestamps();
         });
@@ -30,6 +28,6 @@ class CreateProducts extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('departaments');
     }
 }
