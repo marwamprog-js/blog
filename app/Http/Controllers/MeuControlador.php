3bdbfd2e5dbf963755ee5d0bbab4bf02ccc65060
Marwam Malta <marwamprog.js@gmail.com>
2024-01-03 22:34:57 -0300

    Primeiros teste com Controllers

--- app/Http/Controllers/MeuControlador.php
+++ app/Http/Controllers/MeuControlador.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MeuControlador extends Controller
 {
-    //
+    public function produtos() {
+        return "Meus produtos";
+    }
+
+    public function getNome() {
+        return "Meu nome";
+    }
+
+    public function multiplicar($n1, $n2) {
+        return "Soma: " . ($n1 + $n2);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,51 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ola/sejabemvindo', function() {
-
-    return "Teste";
-});
-
-Route::get('/usuario/{nome}/{n}', function($nome, $n) {
-
-    for($i = 0; $i <= $n; $i++) {
-        echo "Olá! Seja bem vindo $nome! <br>";
-    }
-
-})->where('nome', '[A-Za-z]+')->where('n', '[0-9]+');
-
-Route::prefix('/aplicacao')->group(function() {
-
-    Route::get('/', function() {
-        return view('app');
-    })->name('app');
-
-    Route::get('/user', function() {
-        return view('user');
-    })->name('app.user');
-
-    Route::get('/profile', function() {
-        return view('profile');
-    })->name('app.profile');
-
-});
-
-Route::get('/produtos', function() {
-    echo "<h1>Produtos</h1>";
-    echo "<ol>";
-    echo "<li>Notebook</li>";
-    echo "<li>Impressora</li>";
-    echo "<li>Mouse</li>";
-    echo "</ol>";
-})->name('meusprodutos');
-
-Route::redirect('todosprodutos1', 'produtos', 301);
-Route::get('todosprodutos2', function() {
-    return redirect()->route('meusprodutos');
-});
-
-////////////////////////////
-
-Route::post('/requisicoes', function(Request $request) {
-    return "Hello POST";
-});
+Route::get('produtos', 'MeuControlador@produtos');
+Route::get('nome', 'MeuControlador@getNome');
+Route::get('multiplicar/{n1}/{n2}', 'MeuControlador@multiplicar');
